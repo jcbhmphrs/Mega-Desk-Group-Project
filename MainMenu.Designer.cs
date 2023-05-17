4f976215852879751ba4a535acc85f36118f385c
robin <robin@ROBFAMLAPTOP>
2023-05-17 11:49:14 -0600

    updated searchQuotes

--- MainMenu.Designer.cs
+++ MainMenu.Designer.cs
@@ -32,13 +32,15 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.btnSearchQuotes = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(314, 83);
+            this.button1.Location = new System.Drawing.Point(419, 102);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(139, 36);
+            this.button1.Size = new System.Drawing.Size(185, 44);
             this.button1.TabIndex = 0;
             this.button1.Text = "Add Quote";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,9 +48,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(314, 241);
+            this.button2.Location = new System.Drawing.Point(419, 297);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(139, 37);
+            this.button2.Size = new System.Drawing.Size(185, 46);
             this.button2.TabIndex = 2;
             this.button2.Text = "Exit";
             this.button2.UseVisualStyleBackColor = true;
@@ -56,27 +59,41 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(314, 162);
+            this.button3.Location = new System.Drawing.Point(419, 245);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(139, 36);
+            this.button3.Size = new System.Drawing.Size(185, 44);
             this.button3.TabIndex = 1;
             this.button3.Text = "View All Quotes";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // btnSearchQuotes
+            // 
+            this.btnSearchQuotes.Location = new System.Drawing.Point(419, 178);
+            this.btnSearchQuotes.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSearchQuotes.Name = "btnSearchQuotes";
+            this.btnSearchQuotes.Size = new System.Drawing.Size(185, 46);
+            this.btnSearchQuotes.TabIndex = 3;
+            this.btnSearchQuotes.Text = "Search Quotes";
+            this.btnSearchQuotes.UseVisualStyleBackColor = true;
+            this.btnSearchQuotes.Click += new System.EventHandler(this.btnSearchQuotes_Click);
+            // 
             // MainMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(534, 361);
+            this.ClientSize = new System.Drawing.Size(712, 444);
+            this.Controls.Add(this.btnSearchQuotes);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "MainMenu";
             this.Text = "Main";
@@ -90,6 +107,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnSearchQuotes;
     }
 }
 
---
+++ SearchQuotes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mega_Desk_Group_Project
+{
+    public partial class SearchQuotes : Form
+    {
+        private MainMenu _mainMenu { get; set; }
+        private List<DeskQuote> _quoteList { get; set; }
+        public SearchQuotes(MainMenu mainMenu, List<DeskQuote> quoteList)
+        {
+            InitializeComponent();
+            this.Location = _mainMenu.Location;
+            _mainMenu = mainMenu;
+            _quoteList = quoteList;
+
+        }
+    }
+}
